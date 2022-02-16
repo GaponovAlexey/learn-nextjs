cbbe2bea51c16d9cd29afb6c2a3b3a64bf2ddc66
Kekelberi <alexei.gaponov@gmail.com>
2022-02-16 22:23:17 +0300

    save

--- pages/404.js
+++ pages/404.js
@@ -4,7 +4,7 @@ import style from '../styles/404.module.scss'
 const Error = () => {
   const  router  = useRouter()
   useEffect(() => {
-    setTimeout(() => router.push('/'), 2000)
+    setTimeout(() => router.push('/'), 3000)
   }, [])
 
   return <div className='style'>404 error</div>
