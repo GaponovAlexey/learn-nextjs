2189e33a0b3122c9f3d65360fad1212e54a47f05
Kekelberi <alexei.gaponov@gmail.com>
2022-02-17 20:50:14 +0300

    save

--- pages/Posts/[id].js
+++ pages/Posts/[id].js
@@ -1,6 +1,18 @@
 import Link from 'next/link'
 import Heading from '../../components/Layout/Heading'
 
+const baseURL = 'https://jsonplaceholder.typicode.com/posts'
+
+export const getStaticPaths = async () => {
+  const response = await fetch(baseURL)
+  const data = await response.json()
+  
+  const path = data.map(({id}) => ({
+    params: {}
+  }))
+}
+
+
 const Posts = () => {
   return <>hi</>
 }
